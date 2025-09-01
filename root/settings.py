d310ad6c3dbbe8d534d23393ae0a579777d0f2ad
asilbek3450 <asilbek3450@gmail.com>
2025-09-01 13:54:54 +0500

    static files

--- root/settings.py
+++ root/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'learning' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
